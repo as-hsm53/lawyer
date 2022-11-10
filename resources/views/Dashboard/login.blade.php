93e9e321b809f1eecdddf1f4fb583f1e77e3471f
as-hsm53 <hector.s.moone@gmail.com>
2022-11-10 11:50:39 +0500

    New template Integrated || Login Page Changed

--- resources/views/Dashboard/login.blade.php
+++ resources/views/Dashboard/login.blade.php
@@ -1,104 +1,170 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html class="no-js">
 <head>
+	<title>Justice</title>
+	<meta charset="utf-8">
+	<!--[if IE]>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<![endif]-->
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
-    <title>SB Admin 2 - Login</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/animations.css">
+	<link rel="stylesheet" href="css/fonts.css">
+	<link rel="stylesheet" href="css/main.css" id="color-switcher-link">
+	<script src="js/vendor/modernizr-2.6.2.min.js"></script>
 
-    <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+	<!--[if lt IE 9]>
+		<script src="js/vendor/html5shiv.min.js"></script>
+		<script src="js/vendor/respond.min.js"></script>
+		<script src="js/vendor/jquery-1.12.4.min.js"></script>
+	<![endif]-->
 
 </head>
 
-<body class="bg-gradient-primary">
+<body class="theme-dashboard">
+	<!--[if lt IE 9]>
+		<div class="bg-danger text-center">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/" class="highlight">upgrade your browser</a> to improve your experience.</div>
+	<![endif]-->
 
-    <div class="container">
+	<div class="preloader">
+		<div class="preloader_image"></div>
+	</div>
 
-        <!-- Outer Row -->
-        <div class="row justify-content-center">
+	<!-- search modal -->
+	<div class="modal" tabindex="-1" role="dialog" aria-labelledby="search_modal" id="search_modal">
+		<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+			<span aria-hidden="true">
+				<i class="rt-icon2-cross2"></i>
+			</span>
+		</button>
+		<div class="widget widget_search">
+			<form method="get" class="searchform search-form form-inline" action="https://html.modernwebtemplates.com/justice/">
+				<div class="form-group">
+					<input type="text" value="" name="search" class="form-control" placeholder="Search keyword" id="modal-search-input">
+				</div>
+				<button type="submit" class="theme_button">Search</button>
+			</form>
+		</div>
+	</div>
 
-            <div class="col-xl-10 col-lg-12 col-md-9">
+	<!-- Unyson messages modal -->
+	<div class="modal fade" tabindex="-1" role="dialog" id="messages_modal">
+		<div class="fw-messages-wrap ls with_padding">
+			<!-- Uncomment this UL with LI to show messages in modal popup to your user: -->
+			<!--		
+		<ul>
+			<li>Message To User</li>
+		</ul>
+		-->
 
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
-                        <div class="row">
-                            @if(Session::has("message"))
-                                <div class="col-lg-12">
-                                    <div class=" alert alert-success">{{session("message")}}</div>
-                                </div>
-                            @endif
-                            @if ($errors->any())
-                                <div class="col-lg-12">
-                                    @foreach ($errors->all() as $error)
-                                        <div class="alert alert-danger">{{ $error }}</div>
-                                    @endforeach
-                                </div>
-                            @endif
-                            @if(Session::has("error"))
-                                <div class="col-lg-12">
-                                    <div class=" alert alert-danger">{{session("error")}}</div>
-                                </div>
-                            @endif
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                            <div class="col-lg-6">
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+		</div>
+	</div>
+	<!-- eof .modal -->
+
+	<!-- wrappers for visual page editor and boxed version of template -->
+	<div id="canvas">
+		<div id="box_wrapper">
+
+			<!-- template sections -->
+			<section class="ls ms section_padding_top_100 section_padding_bottom_100 section_full_height">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
+                            <div class="row">
+                                @if(Session::has("message"))
+                                    <div class="col-lg-12">
+                                        <div class=" alert alert-success">{{session("message")}}</div>
                                     </div>
-                                    <form class="user" method="POST" action="auth">
-                                        @csrf
-                                        <div class="form-group">
-                                            <input type="email" class="form-control" name="email" placeholder="Enter Email Address...">
-                                            @if ($errors->has('email'))
-                                            <span class="text-danger">{{ $errors->first('email') }}</span>
-                                            @endif
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="password" class="form-control" name="password" placeholder="Enter Password">
-                                            @if ($errors->has('password'))
-                                            <span class="text-danger">{{ $errors->first('password') }}</span>
-                                            @endif
-                                        </div>
-                                        <hr>
-                                        <button type="submit" class="btn btn-primary btn-user btn-block">Login</button>
-                                    </form>
-                                </div>
+                                @endif
+                                <!-- @if ($errors->any())
+                                    <div class="col-lg-12">
+                                        @foreach ($errors->all() as $error)
+                                            <div class="alert alert-danger">{{ $error }}</div>
+                                        @endforeach
+                                    </div>
+                                @endif -->
+                                @if(Session::has("error"))
+                                    <div class="col-lg-12">
+                                        <div class=" alert alert-danger">{{session("error")}}</div>
+                                    </div>
+                                @endif
                             </div>
-                        </div>
-                    </div>
-                </div>
+							<div class="with_background with_padding">
 
-            </div>
+								<h4 class="text-center">
+									Welcome Back!
+								</h4>
+								<hr class="bottommargin_30">
+								<div class="wrap-forms">
+									<form method="POST" action="auth">
+                                        @csrf
+										
+										<div class="row">
+											<div class="col-sm-12">
+												<div class="form-group has-placeholder">
+													<label for="login-email">Email address</label>
+													<i class="grey fa fa-envelope-o"></i>
+													<input type="email" name="email" class="form-control" id="login-email" placeholder="Email">
+													@if ($errors->has('email'))
+													<span class="text-danger">{{ $errors->first('email') }}</span>
+													@endif
+												</div>
 
-        </div>
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-sm-12">
+												<div class="form-group has-placeholder">
+													<label for="login-password">Password</label>
+													<i class="grey fa fa-pencil-square-o"></i>
+													<input type="password" name="password" class="form-control" id="login-password" placeholder="Password">
+													@if ($errors->has('password'))
+													<span class="text-danger">{{ $errors->first('password') }}</span>
+													@endif
+												</div>
+											</div>
+										</div>
+										<!-- <div class="row">
+											<div class="col-sm-12">
+												<div class="checkbox">
+													<label>
+														<input type="checkbox"> Rememrber Me
+													</label>
+												</div>
+											</div>
+										</div> -->
+										<button type="submit" class="theme_button block_button color1">Login</button>
+									</form>
+								</div>
 
-    </div>
+							</div>
+							<!-- .with_background -->
 
-    <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+							<!-- <p class="divider_20 text-center">
+								Already registered?
+								<a href="admin_signin.html">Log In</a>.
+							</p> -->
 
-    <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+						</div>
+						<!-- .col-* -->
+					</div>
+					<!-- .row -->
+				</div>
+				<!-- .container -->
+			</section>
+		</div>
+		<!-- eof #box_wrapper -->
+	</div>
+	<!-- eof #canvas -->
 
-    <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
+	<script src="js/compressed.js"></script>
+	<script src="js/vendor/Chart.bundle.min.js"></script>
+	<script src="js/main.js"></script>
+	<script src="js/admin-charts.js"></script>
 
 </body>
-
 </html>
